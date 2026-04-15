d16092a5d55836c82cec6fe3ec9333202593297a
dcalpuerto-arch <dcalpuerto@gmail.com>
2026-04-15 08:44:57 +0800

    revision in sql file
    revision in sql file

--- day2/SELECT.sql
+++ day2/SELECT.sql
@@ -1,3 +1,18 @@
+Query 1
+
+SELECT
+s.first_name || ' ' || s.last_name AS full_name,
+c.course_name || ' - ' || c.department AS course_department,
+e.enrollment_date
+FROM students s
+JOIN enrollments e ON s.student_id = e.student_id
+JOIN courses c ON e.course_id = c.course_id
+WHERE s.age > 21
+ORDER BY e.enrollment_date DESC;
+
+
+Query 2
+
 SELECT
 UPPER(s.first_name || ' ' || s.last_name) AS full_name,
 c.course_name || ' - ' || c.department AS course_department,
