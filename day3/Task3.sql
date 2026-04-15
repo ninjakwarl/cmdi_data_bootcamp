047de87d96a8b806779df7cc60ec8ad9b5108a25
sebentinzerofive <jrayestrella@gmail.com>
2026-04-15 15:39:06 +0800

    Update Task3.sql

--- day3/Task3.sql
+++ day3/Task3.sql
@@ -1,9 +1,11 @@
 SELECT 
     d.dept_name AS department,
-    AVG(e.salary) AS average_salary,
+    AVG(e.salary) FILTER (WHERE e.salary >= 50000) AS average_salary,
     COUNT(DISTINCT p.project_id) AS total_projects
 FROM departments d
-LEFT JOIN employees e ON d.dept_id = e.dept_id
-LEFT JOIN projects p ON d.dept_id = p.dept_id
+LEFT JOIN employees e 
+    ON d.dept_id = e.dept_id
+LEFT JOIN projects p 
+    ON d.dept_id = p.dept_id
 GROUP BY d.dept_name
 ORDER BY d.dept_name;
